f4fc7c8359cb1beba03b2941b05e6f8b0a04b2de
valdircarvalho <valdir.carv@gmail.com>
2020-08-28 20:19:37 -0300

    First Commit

---
+++ android/app/src/main/kotlin/com/quizproject/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.quizproject
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Projeto FPA - Quiz',
+    home: TelaBase(),
+  ));
+}
+
+class TelaBase extends StatefulWidget {
+  @override
+  _TelaBaseState createState() => _TelaBaseState();
+}
+
+class _TelaBaseState extends State<TelaBase> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Quiz - Tema a Definir"),
+        centerTitle: true,
+        actions: [IconButton(icon: Icon(Icons.apps), onPressed: null)],
+      ),
+      backgroundColor: Colors.white,
+      body:
+       SingleChildScrollView(
+         padding: EdgeInsets.fromLTRB(20.0, 0, 20.0, 0.0),
+         child: Column(
+           crossAxisAlignment: CrossAxisAlignment.stretch,
+           children: [
+             Icon(
+               Icons.account_circle,
+               size: 80.0,
+             ),
+             TextField(
+               keyboardType: TextInputType.emailAddress,
+               decoration: InputDecoration(labelText: "Insira Seu E-mail"),
+               textAlign: TextAlign.center,
+             ),
+             TextField(
+               keyboardType: TextInputType.emailAddress,
+               decoration: InputDecoration(labelText: "Insira a Senha"),
+               textAlign: TextAlign.center,
+             ),
+             RaisedButton(onPressed: () {},
+               child: Text("Login", style:
+               TextStyle(color: Colors.white, fontSize: 20.0),),
+               color: Colors.blue,
+             ),
+             Text("PPGIA - FPA 2020.1", textAlign: TextAlign.center,),
+           ],
+         ),
+       ),
+    );
+  }
+}
